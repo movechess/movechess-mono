6cb7108e2491fcd6ea8b0e3e26b17ec3509e3d84
thuan.nguyen.van <thuan.nguyen.van@tpssoft.com>
2023-09-06 13:32:53 +0700

    MOV-62

--- packages/movechess-website/src/screens/First-Section.tsx
+++ packages/movechess-website/src/screens/First-Section.tsx
@@ -6,7 +6,7 @@ export default function FirstSection() {
             <div className="bg-[url('/images/background-chess-board.png')] w-full h-full bg-cover bg-center bg-no-repeat mb-8"
             >
                 <div className="px-9 flex flex-col md:flex-row h-full">
-                    <div className="w-full px-9 pt-[45%] sm:pt-[40%] md:pt-[35%] lg:pt-[25%]">
+                    <div className="w-full px-9 pt-[45%] sm:pt-[40%] md:pt-[35%] lg:pt-[25%] text-center">
                         <p className="mt-3 text-4xl md:text-5xl lg:text-6xl font-bold">
                             MoveChess,
                         </p>
@@ -16,16 +16,16 @@ export default function FirstSection() {
                         <p className="py-9 text-xl md:text-2xl lg:text-3xl opacity-70 font-bold">
                             Chess platform on SUI Network
                         </p>
-                        <div className="flex justify-center mb-6 p-3 bg-[#232020] border-2 border-white rounded-2xl content-center shadow-2xl ring-2 ring-gray-200 ring-opacity-10">
+                        <div className="flex justify-center sm:max-w-[80%] lg:max-w-[55%] mx-auto mb-6 p-3 bg-[#232020] border-2 border-white rounded-2xl content-center shadow-2xl ring-2 ring-gray-200 ring-opacity-10">
                             <div className="relative w-full">
-                                <p className="text-center text-xl md:text-2xl lg:text-3xl py-3">Subscribe your address to receive a free-mint MoveChess NFT</p>
+                                <p className="text-center text-sm md:text-lg lg:text-xl py-3">Subscribe your address to receive a free-mint MoveChess NFT</p>
                                 <div className="sm:flex text-center mt-3 mb-5 items-center">
                                     <input
-                                        className="items-center text-sm md:text-2xl lg:text-3xl rounded-full p-4 pl-10 w-full"
+                                        className="items-center text-xs sm:text-sm rounded-full p-4 sm:pl-10 w-full"
                                         type="text"
                                         placeholder="Enter your Sui address"
                                     />
-                                    <button className="text-l text-lg md:text-xl lg:text-2xl font-w sm:absolute rounded-full p-1 sm:p-3 w-full sm:w-[150px] h-[50px] m-[4px] sm:m-[3px] mt-3 sm:mt-0 bg-black sm:bg-[#232020] right-0 border-white shadow-2xl py-2 ring-2 ring-gray-200 ring-opacity-60">
+                                    <button className="text-xs sm:text-sm font-w absolute rounded-full w-[25%] sm:w-[20%] h-[20%] sm:h-[30%] m-[8px] sm:m-[4px] bg-[#232020] right-0 border-white shadow-2xl py-2 ring-2 ring-gray-200 ring-opacity-60">
                                         <p className="opacity-60 font-bold">Submit</p>
                                     </button>
                                 </div>

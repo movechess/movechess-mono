7fcfc3259ca00d446448ac5a011d80b5f689dee8
thuan.nguyen.van <thuan.nguyen.van@tpssoft.com>
2023-09-27 17:11:01 +0700

    MOV-62: Landing page v2

---
+++ packages/movechess-website/src/components/Layout/BackgroundTextLoop.tsx
@@ -0,0 +1,32 @@
+const BackgroundTextLoop: React.FC = () => {
+  return (
+    <>
+      <div className="w-5 h-5 bg-white mx-2"></div>
+      King <div className="w-2 h-5 bg-white mx-2"></div> Queen
+      <div className="w-5 h-5 bg-white mx-2"></div> Rook
+      <div className="w-2 h-5 bg-white mx-2"></div>Bishop
+      <div className="w-5 h-5 bg-white mx-2"></div>Knight
+      <div className="w-2 h-5 bg-white mx-2"></div>Pawn
+      <div className="w-5 h-5 bg-white mx-2"></div>
+      King <div className="w-2 h-5 bg-white mx-2"></div> Queen
+      <div className="w-5 h-5 bg-white mx-2"></div> Rook
+      <div className="w-2 h-5 bg-white mx-2"></div>Bishop
+      <div className="w-5 h-5 bg-white mx-2"></div>Knight
+      <div className="w-2 h-5 bg-white mx-2"></div>Pawn
+      <div className="w-5 h-5 bg-white mx-2"></div>
+      King <div className="w-2 h-5 bg-white mx-2"></div> Queen
+      <div className="w-5 h-5 bg-white mx-2"></div> Rook
+      <div className="w-2 h-5 bg-white mx-2"></div>Bishop
+      <div className="w-5 h-5 bg-white mx-2"></div>Knight
+      <div className="w-2 h-5 bg-white mx-2"></div>Pawn
+      <div className="w-5 h-5 bg-white mx-2"></div>
+      King <div className="w-2 h-5 bg-white mx-2"></div> Queen
+      <div className="w-5 h-5 bg-white mx-2"></div> Rook
+      <div className="w-2 h-5 bg-white mx-2"></div>Bishop
+      <div className="w-5 h-5 bg-white mx-2"></div>Knight
+      <div className="w-2 h-5 bg-white mx-2"></div>Pawn
+    </>
+  )
+}
+
+export default BackgroundTextLoop
--- packages/movechess-website/src/screens/LandingPageV2.tsx
+++ packages/movechess-website/src/screens/LandingPageV2.tsx
@@ -1,115 +1,54 @@
+import BackgroundTextLoop from "../components/Layout/BackgroundTextLoop"
+import Introduction from "../components/Layout/Introduction"
 import Slogan from "../components/Layout/Slogan"
+import { useEffect, useRef, useState } from "react"
 
 const LandingPageV2: React.FC = () => {
+  const [numDivs, setNumDivs] = useState(0)
+  const divLoop = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    const calculateNumDivs = () => {
+      const windowHeight = window.innerHeight
+      let divHeight = 100
+      if (divLoop.current) {
+        divHeight = divLoop.current.clientHeight
+      }
+      const numDivsNeeded = Math.ceil(windowHeight / divHeight)
+      setNumDivs(numDivsNeeded)
+    }
+
+    calculateNumDivs()
+    window.addEventListener("resize", calculateNumDivs)
+
+    return () => {
+      window.removeEventListener("resize", calculateNumDivs)
+    }
+  }, [])
+
   return (
     <>
-      <div className="md:flex justify-center ">
-        <div className="max-w-[1536px] w-[1536px] h-screen min-w-[400px]">
-          <Slogan></Slogan>
-          <div className="md:w-1/2 float-right max-w-[768px] h-[1500px]">
-            <div className="md:pr-16 lg:pr-24 md:pt-16 lg:pt-32 w-full h-full mb-3">
-              <div className="bg-[#212325] w-[90%] md:h-[40%] lg:h-[50%] float-right rounded-[30px] mb-5">
-                <div className="shrink-0 flex justify-center mt-[10%]">
-                  <img
-                    className="w-[90px]"
-                    src={"/images/queen-logo.png"}
-                    alt="logo"
-                  />
+      <div className="lg:flex lg:justify-center ">
+        <div className="max-w-[1536px] lg:w-[1536px] h-screen min-w-[400px]">
+          <div className="flex">
+            <div className="fixed inset-0 flex flex-wrap items-center pointer-events-none overflow-hidden h-screen">
+              {[...Array(numDivs).keys()].map((index) => (
+                <div key={index} ref={divLoop} className="flex-nowrap text-2xl">
+                  <div className="flex opacity-30 overflow-hidden">
+                    <BackgroundTextLoop></BackgroundTextLoop>
+                  </div>
+                  <div className="flex opacity-20">
+                    <BackgroundTextLoop></BackgroundTextLoop>
+                  </div>
+                  <div className="flex opacity-10">
+                    <BackgroundTextLoop></BackgroundTextLoop>
+                  </div>
                 </div>
-                <div className="text-center px-28 mt-5">
-                  <h1 className="text-[32px] font-bold">
-                    Your Chess, Your Narrative
-                  </h1>
-                  <p className="text-[24px] mt-3 opacity-60 text-center">
-                    Delve into a journey with bespoke pieces, face novel
-                    challenges, and traverse a landscape where every move is a
-                    reflection of you
-                  </p>
-                </div>
-                <div className="shrink-0 flex justify-center">
-                  <img
-                    className="w-[300px]"
-                    src={"/images/double-pawn.png"}
-                    alt="logo"
-                  />
-                </div>
-              </div>
-              <div className="bg-[#212325] w-[90%] md:h-[40%] lg:h-[50%] float-right rounded-[30px] mb-5">
-                <div className="shrink-0 flex justify-center mt-[10%]">
-                  <img
-                    className="w-[90px]"
-                    src={"/images/knight-logo.png"}
-                    alt="logo"
-                  />
-                </div>
-                <div className="text-center px-24 mt-5">
-                  <h1 className="text-[32px] font-bold">
-                    Maximize Your Chess Mastery
-                  </h1>
-                  <p className="text-[24px] mt-3 opacity-60 text-center">
-                    Train with our tools, solve complex puzzles, and analyze
-                    deeply. Outplay rivals move by move
-                  </p>
-                </div>
-                <div className="shrink-0 flex justify-center mt-5 ml-12">
-                  <img
-                    className="w-[300px]"
-                    src={"/images/tactic-sheet.png"}
-                    alt="logo"
-                  />
-                </div>
-              </div>
-              <div className="bg-[#212325] w-[90%] md:h-[40%] lg:h-[50%] float-right rounded-[30px] mb-5">
-                <div className="shrink-0 flex justify-center mt-[10%]">
-                  <img
-                    className="w-[90px]"
-                    src={"/images/rook-logo.png"}
-                    alt="logo"
-                  />
-                </div>
-                <div className="text-center px-24 mt-5">
-                  <h1 className="text-[32px] font-bold">
-                    Unlock Exclusive Rewards with In-Game NFTs
-                  </h1>
-                  <p className="text-[24px] mt-3 opacity-60 text-center">
-                    Collect MoveChess NFTs and unlock unmatched rewards
-                  </p>
-                </div>
-                <div className="shrink-0 flex justify-center mt-3 ml-9">
-                  <img
-                    className="w-[300px]"
-                    src={"/images/knight-3d.png"}
-                    alt="logo"
-                  />
-                </div>
-              </div>
-              <div className="bg-[#212325] w-[90%] md:h-[30%] lg:h-[30%] float-right rounded-[30px] mb-5">
-                <div className="shrink-0 flex justify-center mt-[10%]">
-                  <img
-                    className="w-[90px]"
-                    src={"/images/king-logo.png"}
-                    alt="logo"
-                  />
-                </div>
-                <div className="text-center px-24 mt-5">
-                  <h1 className="text-[32px] font-bold">Join our Discord</h1>
-                  <p className="text-[24px] mt-3 opacity-60 text-center">
-                    Where Chess Enthusiasts Connect
-                  </p>
-                </div>
-                <div className="mt-9 flex justify-center">
-                  <button className="flex items-center space-x-2 bg-[#B3FF77] hover:bg-[#83ca4c] text-black px-5 py-3 rounded-full">
-                    <img
-                      className="h-6 md:h-6 w-auto"
-                      src={"/images/discord-logo-black.png"}
-                      alt="icon"
-                    />
-                    <span className="font-bold text-lg">JOIN OUR DISCORD</span>
-                  </button>
-                </div>
-              </div>
+              ))}
+              <div className="bg-black opacity-70 w-full h-full absolute"></div>
             </div>
           </div>
+          <Slogan></Slogan>
+          <Introduction></Introduction>
         </div>
       </div>
     </>
